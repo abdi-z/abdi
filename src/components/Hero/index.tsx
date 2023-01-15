ae24e5c1fff9a5e78c6342d31c8e6f9296f6a803
Abdul Rehman Aziz <abdulrehmanx911@gmail.com>
2023-01-15 23:47:14 +0500

    Feat: Added updated resume

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -109,7 +109,7 @@ export const Hero = () => {
             </a>
           </Button>
         </NextLink>
-        <NextLink href={"/AbdulRehmanAziz.pdf"} passHref={true}>
+        <NextLink href={"/AbdulRehmanAziz1.pdf"} passHref={true}>
           <Button size="sm" as="a" leftIcon={<SiAdobeacrobatreader />}>
             Resume
           </Button>
